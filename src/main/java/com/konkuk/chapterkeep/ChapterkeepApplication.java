873a5eb4320426ea6e71e9d1960d5227cbd1bd64
Joy <munchinthepool64@gmail.com>
2024-09-25 20:49:36 +0900

    Initial commit

---
+++ src/main/java/com/konkuk/chapterkeep/ChapterkeepApplication.java
@@ -0,0 +1,13 @@
+package com.konkuk.chapterkeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChapterkeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChapterkeepApplication.class, args);
+	}
+
+}
