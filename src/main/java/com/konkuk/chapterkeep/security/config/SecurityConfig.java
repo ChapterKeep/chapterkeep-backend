98c8fd93c737ff35c977f9c190abf1dcfdf7ce81
Joy <munchinthepool64@gmail.com>
2024-11-10 17:55:37 +0900

    feat : CORS 설정

--- src/main/java/com/konkuk/chapterkeep/security/config/SecurityConfig.java
+++ src/main/java/com/konkuk/chapterkeep/security/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.konkuk.chapterkeep.security.config;
 
 import com.konkuk.chapterkeep.security.*;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 
 @Configuration
@@ -35,6 +40,37 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // cors 설정
+        http
+                .cors((corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration configuration = new CorsConfiguration();
+
+                        // Origin을 http://localhost:3000 만 허용
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+
+                        // 모든 메서드(GET, POST, PUT, DELETE 등) 허용
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+
+                        // 자격 증명(쿠키, Authorization 헤더 등)을 포함한 요청을 허용
+                        configuration.setAllowCredentials(true);
+
+                        // 모든 HTTP 헤더 허용
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+
+                        // 3600초(1시간) 동안 캐시
+                        configuration.setMaxAge(3600L);
+
+                        // 클라이언트에게 노출할 헤더 설정 - Authorization 헤더를 노출하여 클라이언트가 접근할 수 있게 설정
+                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                        return configuration;
+                    }
+                })));
 
         // csrf disable
         http
